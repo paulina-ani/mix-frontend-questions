681c42bf8418cdfc87b834ddc1d2a86d481f3e06
Paulina <aniskiewicz.paulina@gmail.com>
2019-06-30 10:28:09 +0200

    7th-11th exercise added

--- index.js
+++ index.js
@@ -56,3 +56,71 @@ function changeNumber3() {
   return x3;
 }
 console.log(changeNumber3(5)); // 4
+
+/* 7. True or False
+  Adding primitive type values in JS is done by reference.
+  Answer: False
+*/
+
+/* 8. Arrow function characteristics:
+  a) is anonymous
+  b) may works without "return"
+*/
+
+/* 9a. Which methods are available for array? 
+  a) forEach, forEvery, map, filter, reduce, find
+  b) find, filter, map, forEach, any, some
+  c) forEvery, each, reduce, map
+  d) forEach, filter,map, reduce, some
+  
+  Answer: forEach, filter, map, reduce, some 
+*/
+
+// 9b.
+console.log([2, 5, 8, 1, 4].some(x => x > 10)); // false
+console.log([1, 12, 8, 44, 3].some(x => x > 10)); // true;
+console.log([2, 5, 8, 1, 4].filter(x => x > 10)); // []
+console.log([1, 12, 8, 44, 3].filter(x => x > 10)); // [12, 44]
+
+/* 10a.
+
+Inheritance in JS is being done by:
+  a) class
+  b) prototypes
+
+  Answer: protoypes;
+*/
+// 10b.
+class Car {
+  constructor(name, price, isDiesel) {
+    this.name = name;
+    this.price = price;
+    this.isDiesel = isDiesel;
+    this.turnOn = function() {
+      return "Brum brum brum";
+    };
+  }
+}
+
+let car = new Car("Audi", 35000, true);
+console.log(car.name); // Audi
+console.log(car.turnOn()); // "Brum brum brum"
+console.log(typeof car); // object
+console.log(typeof Car); // function
+
+/*11. Which is correct:
+
+<button id='btn' type='submit'></button>
+const btn = document.getElementById('btn');
+
+  a) btn.addEventListener('click', myFunction());
+  b) btn.addEventListener('click', myFunction);
+  c) btn.click(myFunction());
+  d) btn.click(myFunction);
+
+  Answer: btn.addEventListener('click', myFunction);
+  Example:
+  1) element.addEventListener('click', showSomething)
+  2) element.addEventListener('click', function() {
+    this.style.color = 'red';
+  */
